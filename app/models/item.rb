7b500adb083d2956573ae99792eb77814739fe0c
daichi1997 <tagaaan@gmail.com>
2024-08-21 14:56:29 +0900

    各バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,14 @@ class Item < ApplicationRecord
  belongs_to :charge
  belongs_to :prefecture
  belongs_to :shipping_date
+
+ validates :image,            presence: true
+ validates :name,             presence: true
+ validates :description,      presence: true
+ validates :category_id,      numericality:  { other_than: 1 , message: "can't be blank"}
+ validates :condition_id,     numericality:  { other_than: 1 , message: "can't be blank"}
+ validates :charge_id,        numericality:  { other_than: 1 , message: "can't be blank"}
+ validates :prefecture_id,    numericality:  { other_than: 1 , message: "can't be blank"}
+ validates :shipping_date_id, numericality:  { other_than: 1 , message: "can't be blank"}
+
 end

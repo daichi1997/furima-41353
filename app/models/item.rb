179bd97cf0a38154d80d7b431e8bfed44038af1d
daichi1997 <tagaaan@gmail.com>
2024-08-21 14:49:39 +0900

    active strage導入～各ファイル作成・記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
  belongs_to :user
  has_one_attached :image
 
+ extend ActiveHash::Associations::ActiveRecordExtensions
+ belongs_to :category
+ belongs_to :condition
+ belongs_to :charge
+ belongs_to :prefecture
+ belongs_to :shipping_date
 end

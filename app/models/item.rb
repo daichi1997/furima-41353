d842bcfd1dffbc02866df9bf580ff404a41e7014
daichi1997 <tagaaan@gmail.com>
2024-08-21 20:08:34 +0900

    投稿機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,5 +17,6 @@ class Item < ApplicationRecord
  validates :charge_id,        numericality:  { other_than: 1 , message: "can't be blank"}
  validates :prefecture_id,    numericality:  { other_than: 1 , message: "can't be blank"}
  validates :shipping_date_id, numericality:  { other_than: 1 , message: "can't be blank"}
+ validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end

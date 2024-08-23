cfd346d286ec3d23c2e6ca14fa0789fce734159d
daichi1997 <tagaaan@gmail.com>
2024-08-23 15:34:21 +0900

    一旦テストコード

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,25 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+    @order_add = OrderAdd.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_add = OrderAdd.new(order_params)
+    if @order_add.valid?
+      @order_add.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
   
+  private
+
+  def order_params
+    params.require(:order_add).permit(:postal_code, :prefecture_id, :city, :address_line_main, :address_line_sub, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
 end

f67b899f54196bf004785488878c346ecf9663f7
daichi1997 <tagaaan@gmail.com>
2024-08-28 20:16:06 +0900

    脱字

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.19.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'frima-41353'
+set :application, 'furima-41353'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:daichi1997/furima-41353.git'
